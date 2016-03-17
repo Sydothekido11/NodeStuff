3d14dce7f61ae2019cbcff92d002dba68f688bbc
Sydothekido11 <snem8901@740s-pl-109.local>
2016-03-17 12:24:42 -0600

    Initial Commit

---
+++ NodeStuff/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NodeStuff
+//
+//  Created by Nemelka, Sydney on 3/17/16.
+//  Copyright © 2016 Nemelka, Sydney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
