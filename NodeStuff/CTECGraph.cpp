553759dc747f423b25bb1d1f527d2049e679d83c
Sydothekido11 <snem8901@740s-pl-109.local>
2016-04-25 12:50:10 -0600

    Added the is edge and neighbors

--- NodeStuff/CTECGraph.cpp
+++ NodeStuff/CTECGraph.cpp
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Nemelka, Sydney. All rights reserved.
 //
 
+#include <set>
 #include "CTECGraph.hpp"
 using namespace CTECData;
 
@@ -33,3 +34,58 @@ void CTECGraph<Type> :: addEdge(int source, int target)
     assert(source < size() && target < size());
     adjacencyMatrix[source] [target] = true;
 }
+
+template <class Type>
+bool CTECGraph<Type> :: isEdge(int source, int target) const
+{
+    assert(source < size() && target < size());
+    bool isAnEdge = false;
+    isAnEdge = adjacencyMatrix[source] [target];
+    
+    return isAnEdge;
+}
+
+template <class Type>
+Type& CTECGraph<Type> :: operator[](int vertex)
+{
+    assert(vertex < size());
+    return labels[vertex];
+}
+
+template <class Type>
+Type CTECGraph<Type> :: operator[](int vertex) const
+{
+    assert(vertex < size());
+    return labels[vertex];
+}
+
+template <class Type>
+std::set<int> CTECGraph<Type> :: neighbors(int vertex) const
+{
+ 
+    assert(vertex < size());
+    std::set<int> vertexNeighbors;
+    
+    for(int index = 0; index < size(); index++)
+    {
+        if(adjacencyMatrix[vertex][index])
+        {
+            vertexNeighbors.insert(index);
+        }
+    }
+    return vertexNeighbors;
+}
+
+
+template <class Type>
+void CTECGraph<Type> :: removeEdge(int source, int target)
+{
+    assert(source < size() && target <size());
+    adjacencyMatrix[source] [target] = false;
+}
+
+
+
+
+
+
